84bfe6b4f16e7e52651abc281da1e5f4bdad750f
Александр <aleksandr.misyura@gmail.com>
2015-11-10 12:46:44 +0300

    blog: upd

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ActiveRecord::Base
+  #self.table_name = 'main_articles'
+  #self.primary_key = "some_field_id"
   has_many :comments, dependent: :destroy
   validates :title, presence: true,
             length: { minimum: 5 }
